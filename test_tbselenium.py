791e03258c53379dde587a4bf0c05e0d2bc053ad
Noah Vesely <noah@freedom.press>
2016-04-11 20:23:50 -0700

    Fix test to work with patched tbdriver.py (see NOTICE in diff)

--- test_tbselenium.py
+++ test_tbselenium.py
@@ -1,12 +1,24 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
+
+# NOTICE: this is only working right now because I'm working from a dirty
+# submodule where I've implemented this
+# https://github.com/fowlslegs/tor-browser-selenium/commit/8f7c88871735fc86ee0209595e718ea03841ffee
+# commit
 
 import os
 import site
-import sys
-sys.path.append(os.path.join(os.getcwd(), 'tor-browser-selenium'))
-# site.addsitedir(path.join(getcwd(), 'tor-browser-selenium'))
+site.addsitedir(os.path.join(os.getcwd(), 'tor-browser-selenium'))
 from tbselenium.tbdriver import TorBrowserDriver
 
-with TorBrowserDriver('~/.tb-stable/tor-browser_en-US/') as driver:
+home_dir = os.path.expanduser('~')
+tbb_path = os.path.join(home_dir, 'tbb', 'tor-browser_en-US')
+tbb_fx_path = os.path.join(tbb_path, 'Browser', 'firefox')
+tbb_profile_path = os.path.join(tbb_path, 'Browser', 'TorBrowser', 'Data',
+                                'Browser')
+logfile_path = os.path.join(home_dir, 'FingerprintSecureDrop', 'logging',
+                            'firefox.log')
+
+with TorBrowserDriver(tbb_path=tbb_path,
+                      tbb_logfile_path=logfile_path) as driver:
     driver.get('https://check.torproject.org')
-    sleep(1)  # stay one second in the page
+    time.sleep(1)  # stay one second in the page
