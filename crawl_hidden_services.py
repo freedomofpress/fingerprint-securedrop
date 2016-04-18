43eb5e978391429bb2c91aec8408e3f212795045
Noah Vesely <noah@freedom.press>
2016-04-18 16:40:27 -0700

    Redo configparsing, ship w/ sorter log, disable sorter role

--- crawl_hidden_services.py
+++ crawl_hidden_services.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 
+# Automates visiting sites using Tor Browser running in a virtual X
+# framebuffer. Does not take an arbitrary list of sites, but rather parses a
+# logfile created by running ./sort_hidden_services.py wherein sets of
+# up-and-running hidden services sorted by whether they are SecureDrop or not
+# can be found. Arguments found in the imports/ globals block starting with
+# `import configparse` just below can be set via the ./config.ini file.
+
+from ast import literal_eval
+import re
+from logging import setup_logging
 from time import sleep
 from os import (path, environ)
+
 import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 home_dir = path.expanduser('~')
-fpsd_dir = path.join(home_dir, 'FingerprintSecureDrop')
-hs_sorter_log = path.join(fpsd_dir, 'logging',
-                          config.get('Crawl Hidden Services',
-                                     'hs_sorter_log'))
+fpsd_path = path.join(home_dir,
+                      config.get('Crawl Hidden Services', 'fpsd_path'))
+hs_sorter_log = path.join(fpsd_path, 'logging',
+                          config.get('Crawl Hidden Services', 'hs_sorter_log'))
 tbb_path = path.join(home_dir, config.get('Crawl Hidden Services', 'tbb_path'))
 environ['TBB_PATH'] = tbb_path # Required by tbselenium.test.__init__ checks
 tbb_logfile_path = path.join(fpsd_dir, 'logging', 
                              config.get('Crawl Hidden Services',
                                         'tbb_logfile'))
-
+page_load_timeout = config.get('Crawl Hidden Services', 'page_load_timeout')
 from site import addsitedir
 addsitedir(path.join(fpsd_dir, 'tor-browser-selenium'))
 from tbselenium.test.conftest import (start_xvfb, stop_xvfb)
@@ -23,15 +34,14 @@ from tbselenium.tbdriver import TorBrowserDriver
 from tbselenium.common import USE_RUNNING_TOR
 tor_cfg = USE_RUNNING_TOR
 
-from ast import literal_eval
-import re
 
 class Crawler:
-    def __init__(self, hs_sorter_log):
+    def __init__(self, hs_sorter_log, **kwargs):
         with open(hs_sorter_log, 'r') as logfile:
             logfile = logfile.readlines()
             self.sds = self.extract_set(logfile, -4, 'Up to date SDs:')
             self.not_sds = self.extract_set(logfile, -1, 'Not SDs:')
+        self.page_load_timeout=page_load_timeout
 
     def extract_set(self, hs_sets, subscript, expected_set_prefix):
         line = hs_sets[subscript]
@@ -44,13 +54,14 @@ of the hidden service sorter script.'.format(set_prefix)
         # into a set
         return set(literal_eval(set_str.strip('{}')))
 
-    def crawl_url_set(self, url_set, **kwargs):
-        for url in url_set:
-            with VirtTBDriver(**kwargs) as driver:
-                driver.get(url)
-                driver.get_screenshot_as_file(path.join(fpsd_dir,'logging',
-                                                        '{}.png'.format(url)[7:]))
-                sleep(1)
+    def crawl_url_sets(self, url_set_list, **kwargs):
+        for url_set in url_set_list:
+            for url in url_set:
+                with VirtTBDriver(**kwargs) as driver:
+                    logger.info("Starting crawl of {}".format(url))
+                    driver.get(url)
+                    logger.info("Stopping crawl of {}".format(url))
+                    sleep(2)
 
 from contextlib import contextmanager
 @contextmanager
@@ -60,8 +71,10 @@ def VirtTBDriver(**kwargs):
     stop_xvfb(virt_framebuffer)
 
 if __name__ == '__main__':
-    crawler = Crawler(hs_sorter_log=hs_sorter_log,)
-    crawler.crawl_url_set(crawler.not_sds,
-                          tbb_path=tbb_path,
-                          tbb_logfile_path=tbb_logfile_path,
-                          tor_cfg=tor_cfg)
+    logger = setup_logging('crawler')
+    crawler = Crawler(hs_sorter_log=hs_sorter_log,
+                      page_load_timeout=page_load_timeout)
+    crawler.crawl_url_sets([crawler.not_sds, crawler.sds],
+                            tbb_path=tbb_path,
+                            tbb_logfile_path=tbb_logfile_path,
+                            tor_cfg=tor_cfg)
