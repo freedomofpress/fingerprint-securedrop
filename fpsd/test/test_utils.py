f3187499a19b2656e44f97e274ea63c996a742a5
redshiftzero <jen@redshiftzero.com>
2016-08-30 16:33:53 -0700

    Add tests for new utils

---
+++ fpsd/test/test_utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.5
+
+import unittest
+import datetime
+
+from utils import get_lookback
+
+
+class LookbackTimeTest(unittest.TestCase):
+    def test_one_week(self):
+        self.assertEqual(get_lookback('1w'), datetime.timedelta(7))
+
+    def test_four_weeks(self):
+        self.assertEqual(get_lookback('4w'), datetime.timedelta(28))
+
+    def test_one_month(self):
+        with self.assertRaises(TypeError):
+            lookback_time = get_lookback('1m')
+
+    def test_no_units(self):
+        with self.assertRaises(TypeError):
+            lookback_time = get_lookback('666')
