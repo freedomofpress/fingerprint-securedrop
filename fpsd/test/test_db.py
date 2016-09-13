57fc5306d71b8a24818306e97c186a2d3ffe62fc
Noah Vesely <noah@freedom.press>
2016-09-13 15:30:32 -0700

    Fix sorter part of db test

--- fpsd/test/test_db.py
+++ fpsd/test/test_db.py
@@ -2,7 +2,7 @@
 # using the database for storage and retrieval.
 
 from collections import OrderedDict
-from datetime import datetime as dt
+from datetime import timedelta
 import unittest
 
 from crawler import Crawler
@@ -18,30 +18,30 @@ class DatabaseTest(unittest.TestCase):
     class_tests = OrderedDict(
         sd_038="'Powered by SecureDrop 0.3.8' in text", 
         out_of_date="'Powered by SecureDrop' in text")
-    start_sort_time = dt.now() - get_lookback("1h")
+    timespan = timedelta(minutes=3)
 
     def setUp(self):
         self.db_handler = RawStorage()
 
     def test_sort_securedrop_directory(self):
-        with Sorter(use_database=self.use_database) as sortbot9k:
+        with Sorter(db_handler=self.db_handler) as sortbot9k:
             sortbot9k.scrape_directories(self.sd_directory)
             sortbot9k.sort_onions(self.class_tests)
 
         uptodate_class, uptodate_name = \
-                self.db_handler.get_onion_class(self.start_sort_time, True)
+                self.db_handler.get_onion_class(self.timespan, True)
         # At least 10 of our instances should be on the latest version
         self.assertGreater(len(uptodate_class), 9)
-        self.assertRegex(uptodate_class.keys()[0], "http")
-        self.assertRegex(uptodate_class.keys()[0], ".onion")
+        self.assertRegex(list(uptodate_class)[0], "http")
+        self.assertRegex(list(uptodate_class)[0], ".onion")
         self.assertEqual(uptodate_name, "sd_038")
 
         outofdate_class, outofdate_name = \
-                self.db_handler.get_onion_class(self.start_sort_time, False)
+                self.db_handler.get_onion_class(self.timespan, False)
         # At least 2 of our instances will be lagging behind versions :'(
         self.assertGreater(len(outofdate_class), 1)
-        self.assertRegex(uptodate_class.keys()[0], "http")
-        self.assertRegex(uptodate_class.keys()[0], ".onion")
+        self.assertRegex(list(outofdate_class)[0], "http")
+        self.assertRegex(list(outofdate_class)[0], ".onion")
         self.assertEqual(outofdate_name, "out_of_date")
 
 
