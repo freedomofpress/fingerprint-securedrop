b221b3938ce43fc32323f22e14f02da54585b239
redshiftzero <jen@redshiftzero.com>
2016-09-21 13:49:29 -0700

    Add generic function for getting feature importances of trained model

---
+++ fpsd/evaluation.py
@@ -0,0 +1,15 @@
+def get_feature_importances(model):
+
+    try:
+        return model.feature_importances_
+    except:
+        pass
+    try:
+        # Must be 1D for feature importance plot
+        if len(model.coef_) <= 1:
+            return model.coef_[0]
+        else:
+            return model.coef_
+    except:
+        pass
+    return None
