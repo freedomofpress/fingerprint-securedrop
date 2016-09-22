4a1dbc7a35579636c797b7e807b2fd78e84f6ba8
Noah Vesely <noah@freedom.press>
2016-09-21 19:05:06 -0700

    Omit self for TestCase fixture attrs outside setUp

--- fpsd/test/test_sketchy_sites.py
+++ fpsd/test/test_sketchy_sites.py
@@ -6,12 +6,12 @@ from crawler import Crawler
 
 class CrawlBadSitesTest(unittest.TestCase):
 
-    self.bad_sites = ["http://jlve2diknf45qwjv.onion/",
-                      "http://money2mxtcfcauot.onion",
-                      "http://22222222aziwzse2.onion",
-                      "http://xnsoeplvch4fhk3s.onion",
-                      "http://uptgsidhuvcsquoi.onion",
-                      "http://cubie3atuvex2gdw.onion"]
+    bad_sites = ["http://jlve2diknf45qwjv.onion/",
+                 "http://money2mxtcfcauot.onion",
+                 "http://22222222aziwzse2.onion",
+                 "http://xnsoeplvch4fhk3s.onion",
+                 "http://uptgsidhuvcsquoi.onion",
+                 "http://cubie3atuvex2gdw.onion"]
 
     def test_crawl_of_bad_sites(self):
         with Crawler(restart_on_sketchy_exception=True) as crawler:
